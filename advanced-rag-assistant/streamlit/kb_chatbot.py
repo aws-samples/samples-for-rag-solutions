47e28351071d6b10fb3258fba4989cb34c4e0c7e
Vivek Mittal <vivekmittal514@gmail.com>
2025-02-11 00:25:00 +0000

    Updated to pre signed URL. Added logic to display images in citations

--- advanced-rag-assistant/streamlit/kb_chatbot.py
+++ advanced-rag-assistant/streamlit/kb_chatbot.py
@@ -5,7 +5,7 @@ import os
 import base64
 
 region = boto3.Session().region_name
-session = boto3.Session(region_name=region)
+session = boto3.Session(region_name='us-east-1')
 lambda_client = session.client('lambda')
 
 #Replace with your Lambda function that will be invoking Bedrock Knowledge bases
@@ -25,6 +25,17 @@ def download_s3_file(s3_uri):
     local_file_name = key.split('/')[-1]
     s3.download_file(bucket_name, key, local_file_name)
     return local_file_name
+
+# Add a function to create S3 presigned URL for a S3 object file based on S3 path
+#Change below function to just accept S3 URI and expiration date
+def create_presigned_url(s3_uri, expiration=3600):
+    s3 = boto3.client('s3')
+    bucket_name, key = s3_uri.split('/', 2)[-1].split('/', 1)
+    response = s3.generate_presigned_url('get_object',
+                                          Params={'Bucket': bucket_name,
+                                                  'Key': key},
+                                          ExpiresIn=expiration)
+    return response
 
 
 st.title("RAG Chatbot Assistant")
@@ -51,6 +62,7 @@ if prompt := st.chat_input("What is up?"):
 
     # Display user input in chat message container
     question = prompt
+    #st.chat_message("user").markdown(question)
     st.chat_message("user").markdown(question)
 
     # Call lambda function to get response from the model
@@ -79,15 +91,47 @@ if prompt := st.chat_input("What is up?"):
         # Loop over the citations list and display each citation in a separate chat message
         for citation in citations:
             display_text = citation['generatedResponsePart']['textResponsePart']['text']
-            st.markdown(display_text)
+            # Display the value in display_text such that new lines are also printed as actual new line          
+            #st.markdown(display_text)
+            st.markdown(display_text.replace('\n', '<br>'), unsafe_allow_html=True)
+            #st.text(display_text)
+            #st.markdown(f"<pre>{display_text}</pre>", unsafe_allow_html=True)
+            #formatted_text = display_text.replace(' ', '&nbsp;').replace('\n', '<br>')
+            #st.markdown(formatted_text, unsafe_allow_html=True)
+            # Replace line 94 with:
+            #st.markdown(f"<pre style='white-space: pre-wrap;'>{display_text}</pre>", unsafe_allow_html=True)
+
+
+
+
             display_link=''
             for reference in citation['retrievedReferences']:
                 #increment count variable
                 url = reference['location']['s3Location']['uri']
-                help_text=reference['content']['text']
-                s3_download = download_s3_file(url)
-                download_url = create_download_link(s3_download, s3_download.split('/')[-1])
-                st.markdown(download_url, unsafe_allow_html=True, help=help_text)
+                image = None
+                #If reference['content'] is empty check, get the value of image object from reference[metadata][x-amz-bedrock-kb-byte-content-source]
+                if 'text' not in reference['content']:
+                    image = reference['metadata']['x-amz-bedrock-kb-byte-content-source']
+                    help_text=reference['metadata']['x-amz-bedrock-kb-description']
+                    # Generate presigned url for image S3 object stored in image variable
+                    image = create_presigned_url(image)
+
+                else:
+                    help_text=reference['content']['text']
+                
+                #s3_download = download_s3_file(url)
+                #download_url = create_download_link(s3_download, s3_download.split('/')[-1])
+                presigned_url = create_presigned_url(url);
+                download_url = f"[Doc download link]({presigned_url})"
+
+                #Show image if image object is not empty
+                if image:
+                    image_help = "![Image](" + image + ")" 
+                    st.markdown(download_url, unsafe_allow_html=True, help=image_help)
+                else:
+                    st.markdown(download_url, unsafe_allow_html=True, help=help_text)
+                
 
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": answer})
+
